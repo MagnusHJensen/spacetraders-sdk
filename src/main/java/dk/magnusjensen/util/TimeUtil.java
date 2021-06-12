8f7fbb5c45cd668cc0d89f2fc806981369e3edc5
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-06-12 16:28:51 +0200

    added timeutil, to parse datetime (wip)

---
+++ src/main/java/dk/magnusjensen/util/TimeUtil.java
@@ -0,0 +1,14 @@
+package dk.magnusjensen.util;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeUtil {
+
+	private static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+			.ofPattern("yyyy-MM-dd'T'HH:mm:ss:SSS");
+
+	public static ZonedDateTime parseDateTimeString(String datetime) {
+		return ZonedDateTime.parse(datetime, DATE_TIME_FORMATTER);
+	}
+}
