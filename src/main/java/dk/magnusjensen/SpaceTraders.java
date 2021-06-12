7ca2976f5858f273c3b8e1215fc62ce81f7f94a3
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-06-12 16:26:09 +0200

    updated master class/builder, along with how the Api works, and handles all api calls

--- src/main/java/dk/magnusjensen/SpaceTraders.java
+++ src/main/java/dk/magnusjensen/SpaceTraders.java
@@ -1,20 +1,45 @@
 package dk.magnusjensen;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.magnusjensen.api.ApiCaller;
-import okhttp3.OkHttpClient;
+import dk.magnusjensen.entities.AccountEntity;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceTraders {
 
-	private ApiCaller API = new ApiCaller();
-
-
+	private static ObjectMapper MAPPER = new ObjectMapper();
 	private String username;
 	private String token;
+	private AccountEntity account;
 
-	public SpaceTraders(String username, String token) {
+	protected SpaceTraders(String username, String token, boolean claimUsername) throws Exception {
+		if (claimUsername) {
+			this.claimUsername(username);
+		} else {
+			this.init(token);
+		}
+	}
+
+	private void claimUsername(String username) throws Exception {
+		JsonNode response = ApiCaller.GET("users/:username/claim", null, new ArrayList<>(List.of(username)));
 		this.username = username;
+		this.token = response.get("token").asText();
+		this.account = AccountEntity.getInfo(this.token);
+	}
+
+	private void init(String token) throws Exception {
 		this.token = token;
+		this.account = AccountEntity.getInfo(this.token);
+		this.username = this.account.getUsername();
+	}
+
+	public static String getGameStatus() throws Exception {
+		JsonNode response = ApiCaller.GET("game/status", null, null);
+		return response.get("status").asText();
 	}
 
 	public String getUsername() {
@@ -25,25 +50,37 @@ public class SpaceTraders {
 		return token;
 	}
 
+	@Nullable
+	public AccountEntity getAccount() {
+		return account;
+	}
+
+	public static ObjectMapper getMapper() {
+		return MAPPER;
+	}
 
 
 
-	public class Builder {
+
+	public static class Builder {
 		private String username;
 		private String token;
+		private boolean claim;
 
 		public Builder setUsername(String username) {
 			this.username = username;
+			this.claim = true;
 			return this;
 		}
 
 		public Builder setToken(String token) {
 			this.token = token;
+			this.claim = false;
 			return this;
 		}
 
-		public SpaceTraders build() {
-			return new SpaceTraders(username, token);
+		public SpaceTraders build() throws Exception {
+			return new SpaceTraders(username, token, claim);
 		}
 	}
 }
--- src/main/java/dk/magnusjensen/api/ApiCaller.java
+++ src/main/java/dk/magnusjensen/api/ApiCaller.java
@@ -1,17 +1,119 @@
 package dk.magnusjensen.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dk.magnusjensen.SpaceTraders;
+import dk.magnusjensen.errors.ErrorHandler;
 import dk.magnusjensen.errors.TokenNotSetException;
-import okhttp3.OkHttpClient;
+import okhttp3.*;
 
-public class ApiCaller {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	private ObjectMapper MAPPER = new ObjectMapper();
-	private OkHttpClient CLIENT = new OkHttpClient();
-	private String BASE_URL = "https://api.spacetraders.io/";
+public abstract class ApiCaller {
+
+	private static OkHttpClient CLIENT = new OkHttpClient();
+	private static String BASE_URL = "https://api.spacetraders.io/";
+	private static Pattern URL_PATTERN = Pattern.compile(":.+?(?=/)");
 
 
-	public static GET() {
+	public static JsonNode GET(String endpoint, String token, ArrayList<String> params) throws Exception {
+		endpoint = replaceEndpoint(endpoint, params);
+		Request request = new Request.Builder()
+				.url(BASE_URL + endpoint)
+				.get()
+				.build();
 
+		request = setTokenHeader(request, token);
+
+		return makeCall(request);
+	}
+
+	public static JsonNode POST(String endpoint, String token, ArrayList<String> params, RequestBody body) throws Exception {
+		endpoint = replaceEndpoint(endpoint, params);
+		Request request = new Request.Builder()
+				.url(BASE_URL + endpoint)
+				.post(body)
+				.build();
+
+		request = setTokenHeader(request, token);
+
+		return makeCall(request);
+	}
+
+	public static JsonNode PUT(String endpoint, String token, ArrayList<String> params, RequestBody body) throws Exception {
+		endpoint = replaceEndpoint(endpoint, params);
+		Request request = new Request.Builder()
+				.url(BASE_URL + endpoint)
+				.put(body)
+				.build();
+
+		request = setTokenHeader(request, token);
+
+		return makeCall(request);
+	}
+
+	public static JsonNode DELETE(String endpoint, String token, ArrayList<String> params, RequestBody body) throws Exception {
+		endpoint = replaceEndpoint(endpoint, params);
+		Request request = new Request.Builder()
+				.url(BASE_URL + endpoint)
+				.delete(body)
+				.build();
+
+		request = setTokenHeader(request, token);
+
+		return makeCall(request);
+	}
+
+	private static JsonNode makeCall(Request request) throws Exception {
+		Call call = CLIENT.newCall(request);
+
+		try {
+			Response response = call.execute();
+			JsonNode node =  SpaceTraders.getMapper().readValue(response.body().string(), JsonNode.class);
+
+			if (node.has("error")) {
+				JsonNode errorNode = node.get("error");
+				System.out.println(errorNode.get("message").asText());
+				ErrorHandler.handleError(errorNode);
+			}
+
+			return node;
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+
+		throw new Exception("API " + request.method() + " request has failed!");
+	}
+
+	private static String replaceEndpoint(String endpoint, ArrayList<String> params) {
+		int i = 0;
+
+		Matcher matcher = URL_PATTERN.matcher(endpoint);
+		StringBuffer sb = new StringBuffer();
+		while(matcher.find()) {
+			matcher.appendReplacement(sb, params.get(i));
+			i++;
+		}
+		matcher.appendTail(sb);
+
+		return sb.toString();
+
+	}
+
+	private static Request setTokenHeader(Request request, String token) {
+		if (token != null) {
+			if (!token.equals("")) {
+				request = request.newBuilder()
+						.addHeader("Authorization", "Bearer " + token)
+						.build();
+			}
+		}
+
+		return request;
 	}
 }
